575804ee7e3ff03fb8185e5f3025f19b1a0bbac8
Anton Shustikov <ShustikovAnimation@gmail.com>
2024-01-20 18:32:57 -0500

    Add RabbitMQ impl

--- dispatcher/src/main/java/com/acorn/service/impl/UpdateProducerImpl.java
+++ dispatcher/src/main/java/com/acorn/service/impl/UpdateProducerImpl.java
@@ -1,17 +1,29 @@
 package com.acorn.service.impl;
 
 import com.acorn.service.UpdateProducer;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class UpdateProducerImpl implements UpdateProducer {
 
+    private final RabbitTemplate rabbitTemplate;
+
+
+    /***
+     *  Не забыть добавить в конфигурационный класс jsonMessageConverter,
+     *  convertAndSend под капотом использует @Bean jsonMessageConverter,
+     *  описанный в конфигарацонным файле
+     */
     @Override
     public void produce(String rabbitQueue, Update update) {
         log.debug(update.getMessage().getText());
+        //rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 }
