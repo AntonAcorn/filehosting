8155e2ba655f8075dc50443365d02c2593d04edc
Anton Shustikov <ShustikovAnimation@gmail.com>
2024-01-28 12:44:01 -0500

    MessageConstants refactor

--- node/src/main/java/com/acorn/service/comands/MessageConstants.java
+++ node/src/main/java/com/acorn/service/comands/MessageConstants.java
@@ -4,7 +4,6 @@ public class MessageConstants {
 
     public static final String FEATURE_IN_PROGRESS_COMMAND_ANSWER = "This feature is in progress";
     public static final String HELP_COMMAND_ANSWER = "You can choose one of the following commands: /start, /help, /register";
-    //TODO: Rewrite with dynamic name
-    public static final String START_COMMAND_ANSWER = "Hello in the file hosting";
-    public static final String UNKNOWN_COMMAND_ANSWER = "Hello in the file hosting";
+    public static final String START_COMMAND_ANSWER = "Hello there in the file hosting. Please, use the help command: /help";
+    public static final String UNKNOWN_COMMAND_ANSWER = "The command is unknown. Please, use the /help command";
 }
