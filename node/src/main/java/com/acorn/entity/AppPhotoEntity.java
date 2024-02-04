f550fc8e28eebc362d6bdbea2643d8e0c36ea7c0
Anton Shustikov <shustikovanimation@gmail.com>
2024-02-04 12:38:36 -0500

    Add AppPhotoEntity

---
+++ node/src/main/java/com/acorn/entity/AppPhotoEntity.java
@@ -0,0 +1,62 @@
+package com.acorn.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldNameConstants;
+import org.hibernate.proxy.HibernateProxy;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Getter
+@Setter
+@FieldNameConstants
+@Entity
+@Table(name = "app_photo")
+public class AppPhotoEntity extends AutoIncrementedEntity {
+    @Column(name = "file_id")
+    private String fileId;
+
+    @Column(name = "file_name")
+    private String fileName;
+
+    @Column(name = "mime_type")
+    private String mimeType;
+
+    @Column(name = "file_size")
+    private Long fileSize;
+
+    @OneToOne
+    private BinaryContentEntity binaryContent;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        Class<?> oEffectiveClass = o instanceof HibernateProxy hibernateProxy
+                ? hibernateProxy.getHibernateLazyInitializer().getPersistentClass()
+                : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy hibernateProxy
+                ? hibernateProxy.getHibernateLazyInitializer().getPersistentClass()
+                : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) {
+            return false;
+        }
+        AccountEntity that = (AccountEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy hibernateProxy
+                ? hibernateProxy.getHibernateLazyInitializer().getPersistentClass().hashCode()
+                : getClass().hashCode();
+    }
+}
